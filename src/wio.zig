c1d430efbd93d597f1e2f483bd592f28af7eea6c
Elaine Gibson <ypsvlq@gmail.com>
2025-02-01 13:03:52 +0000

    macos: rename from cocoa

--- src/wio.zig
+++ src/wio.zig
@@ -2,7 +2,7 @@ const std = @import("std");
 const builtin = @import("builtin");
 pub const backend = switch (builtin.os.tag) {
     .windows => @import("win32.zig"),
-    .macos => @import("cocoa.zig"),
+    .macos => @import("macos.zig"),
     .linux, .openbsd, .netbsd, .freebsd, .dragonfly => @import("unix.zig"),
     else => if (builtin.target.isWasm()) @import("wasm.zig") else @compileError("unsupported platform"),
 };
