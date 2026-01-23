60d44fc26a1945702c25804ceb551f09b9cb9547
Elaine Gibson <ypsvlq@gmail.com>
2026-01-23 20:13:24 +0000

    example: use new io interface

--- example/wasm.zig
+++ example/wasm.zig
@@ -3,13 +3,17 @@ const std = @import("std");
 extern "log" fn write([*]const u8, usize) void;
 extern "log" fn flush() void;
 
-fn logWriteFn(_: void, bytes: []const u8) !usize {
-    write(bytes.ptr, bytes.len);
-    return bytes.len;
+fn drain(_: *std.Io.Writer, data: []const []const u8, _: usize) !usize {
+    write(data[0].ptr, data[0].len);
+    return data[0].len;
 }
 
+var writer = std.Io.Writer{
+    .vtable = &.{ .drain = drain },
+    .buffer = &.{},
+};
+
 pub fn logFn(comptime level: std.log.Level, comptime scope: @TypeOf(.enum_literal), comptime format: []const u8, args: anytype) void {
-    const writer = std.io.GenericWriter(void, error{}, logWriteFn){ .context = {} };
     const prefix = if (scope == .default) ": " else "(" ++ @tagName(scope) ++ "): ";
     writer.print(level.asText() ++ prefix ++ format ++ "\n", args) catch {};
     flush();
