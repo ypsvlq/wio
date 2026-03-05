71000d3da94adcd05d4f5cf9bd94c4a8a74baaf4
Elaine Gibson <ypsvlq@gmail.com>
2026-03-05 11:35:31 +0000

    vulkan: replace lossyCast with truncate

--- example/vulkan/src/main.zig
+++ example/vulkan/src/main.zig
@@ -337,8 +337,8 @@ fn recreateSwapchain() !void {
 
     const capabilities = try instance.getPhysicalDeviceSurfaceCapabilitiesKHR(physical_device, surface);
 
-    size.width = std.math.lossyCast(u16, std.math.clamp(size.width, capabilities.min_image_extent.width, capabilities.max_image_extent.width));
-    size.height = std.math.lossyCast(u16, std.math.clamp(size.height, capabilities.min_image_extent.height, capabilities.max_image_extent.height));
+    size.width = @truncate(std.math.clamp(size.width, capabilities.min_image_extent.width, capabilities.max_image_extent.width));
+    size.height = @truncate(std.math.clamp(size.height, capabilities.min_image_extent.height, capabilities.max_image_extent.height));
 
     swapchain = try device.createSwapchainKHR(&.{
         .surface = surface,
