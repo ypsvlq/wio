f4f19b2c179a446db2dccf7a7fed051ed08318a3
Elaine Gibson <ypsvlq@gmail.com>
2026-04-20 12:14:24 +0100

    vulkan: formatting

--- examples/vulkan/src/vertex.zig
+++ examples/vulkan/src/vertex.zig
@@ -3,13 +3,13 @@ const gpu = std.gpu;
 
 const v_color = @extern(*addrspace(.output) @Vector(3, f32), .{ .name = "v_color", .decoration = .{ .location = 0 } });
 
-const positions = [3]@Vector(4, f32){
+const positions: [3]@Vector(4, f32) = .{
     .{ 0, -0.5, 0, 1 },
     .{ 0.5, 0.5, 0, 1 },
     .{ -0.5, 0.5, 0, 1 },
 };
 
-const colors = [_]@Vector(3, f32){
+const colors: [3]@Vector(3, f32) = .{
     .{ 1, 0, 0 },
     .{ 0, 1, 0 },
     .{ 0, 0, 1 },
