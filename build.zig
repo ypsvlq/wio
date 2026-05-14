94266228974da8f38acf2691103e9d4db5e2331f
Elaine Gibson <ypsvlq@gmail.com>
2026-05-14 21:59:53 +0100

    build: remove wio-wayland.h

--- build.zig
+++ build.zig
@@ -143,7 +143,19 @@ pub fn build(b: *std.Build) !void {
                 if (enable_wayland) {
                     if (!system_integration) {
                         try cimport.appendSlice(b.allocator,
-                            \\#include <wio-wayland.h>
+                            \\#include <wayland-client-core.h>
+                            \\extern uint32_t (*wio_wl_proxy_get_version)(struct wl_proxy *);
+                            \\extern struct wl_proxy *(*wio_wl_proxy_marshal_flags)(struct wl_proxy *, uint32_t, const struct wl_interface *, uint32_t, uint32_t, ...);
+                            \\extern int (*wio_wl_proxy_add_listener)(struct wl_proxy *, void (**)(void), void *);
+                            \\extern void (*wio_wl_proxy_destroy)(struct wl_proxy *);
+                            \\extern void (*wio_wl_proxy_set_user_data)(struct wl_proxy *, void *);
+                            \\extern void *(*wio_wl_proxy_get_user_data)(struct wl_proxy *);
+                            \\#define wl_proxy_get_version wio_wl_proxy_get_version
+                            \\#define wl_proxy_marshal_flags wio_wl_proxy_marshal_flags
+                            \\#define wl_proxy_add_listener wio_wl_proxy_add_listener
+                            \\#define wl_proxy_destroy wio_wl_proxy_destroy
+                            \\#define wl_proxy_set_user_data wio_wl_proxy_set_user_data
+                            \\#define wl_proxy_get_user_data wio_wl_proxy_get_user_data
                             \\#include <wayland-protocol.c>
                             \\
                         );
